1f2220b816c445db8b109dd05d0f388ad722c78b
Taylor Moe <taylormoe93@gmail.com>
2022-08-07 12:28:54 -0500

    completed POST

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,5 +8,28 @@ function onReady(){
     // taskComplete button
     // taskDelete button
 
-    //getTasks();
-}
+    // getTasks(); // doesn't exist yet but will be made
+};
+
+function addTask() {
+    let objectToSend = {
+        task: $( '#inputTask' ).val()
+    };
+
+    console.log( 'in addTask:', objectToSend ); // he lives!
+
+    // send to server
+    $.ajax({
+        type: 'POST',
+        url: '/tasks', //double check this if it throws an error. Might be just /.
+        data: 'objectToSend'
+    }).then( function( response ){
+        console.log( 'back from POST:', response );
+        //clear inputs
+        $( 'input' ).val('');
+        //refresh data and DOM
+        // getTasks(); DON'T FORGET ME!
+    }).catch( function( err ){
+        alert( 'error adding item:', err );
+    })
+} // end addTask
