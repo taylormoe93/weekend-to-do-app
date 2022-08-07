a6c09bd70248311881716caba2e529329a2ed957
Taylor Moe <taylormoe93@gmail.com>
2022-08-07 11:54:04 -0500

    initial onReady setup

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,12 @@
 //CLIENT.JS
+
+$( document ).ready( onReady );
+
+function onReady(){
+    //event listeners 
+    $( '#addTaskButton' ).on( 'click', addTask ); //links to addTask function
+    // taskComplete button
+    // taskDelete button
+
+    //getTasks();
+}
